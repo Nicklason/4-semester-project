18ebcb285c7637d85e275e07db4086e2135eefff
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-03-17 16:32:19 +0100

    create gdx application

---
+++ Core/src/main/java/dk/sdu/se4/core/Game.java
@@ -0,0 +1,29 @@
+package dk.sdu.se4.core;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class Game implements ApplicationListener {
+    @Override
+    public void create() {}
+
+    @Override
+    public void render() {
+        // Clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {}
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void dispose() {}
+}
