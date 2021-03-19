4715ac6d16b0f3cbd4be16d1af1da55fe59ed85a
stped18 <stped18@student.sdu.dk>
2021-03-19 08:50:42 +0100

    GameData

--- Common/src/main/java/dk/sdu/se4/common/data/GameData.java
+++ Common/src/main/java/dk/sdu/se4/common/data/GameData.java
@@ -10,6 +10,45 @@ package dk.sdu.se4.common.data;
  * @author steff
  */
 public class GameData {
-    private float deltatime;
+    private float delta;
+    private int displayWidth;
+    private int displayHeight;
+    private GameData gameDataInstance = null;
+    
+    private GameData(){
+        
+    }
+    public GameData getInstance(){
+        if(this.gameDataInstance==null){
+            this.gameDataInstance= new GameData();
+        }
+        return this.gameDataInstance;
+    }
+
+    public float getDelta() {
+        return delta;
+    }
+
+    public void setDelta(float delta) {
+        this.delta = delta;
+    }
+
+    public int getDisplayWidth() {
+        return displayWidth;
+    }
+
+    public void setDisplayWidth(int displayWidth) {
+        this.displayWidth = displayWidth;
+    }
+
+    public int getDisplayHeight() {
+        return displayHeight;
+    }
+
+    public void setDisplayHeight(int displayHeight) {
+        this.displayHeight = displayHeight;
+    }
+    
+    
     
 }
