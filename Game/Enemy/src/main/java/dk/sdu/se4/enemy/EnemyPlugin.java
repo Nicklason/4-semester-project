278d88c78839518c1fab8fc4791692f2402216d9
stped18 <stped18@student.sdu.dk>
2021-03-28 19:13:15 +0200

    fix loading

--- Game/Enemy/src/main/java/dk/sdu/se4/enemy/EnemyPlugin.java
+++ Game/Enemy/src/main/java/dk/sdu/se4/enemy/EnemyPlugin.java
@@ -7,11 +7,15 @@ package dk.sdu.se4.enemy;
 
 import dk.sdu.se4.common.data.GameSpace;
 import dk.sdu.se4.common.services.IPluginService;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.lookup.ServiceProviders;
 
 /**
  *
  * @author steff
  */
+@ServiceProviders(value = {
+    @ServiceProvider(service = IPluginService.class),})
 public class EnemyPlugin implements IPluginService{
 
     @Override
