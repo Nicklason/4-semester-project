36c9a3f0405a31197940b3db3f9002bd9ae83149
stped18 <stped18@student.sdu.dk>
2021-04-15 14:14:26 +0200

    new desing

--- dk.sdu.se4.core/src/main/java/dk/sdu/se4/core/StartMenu.java
+++ dk.sdu.se4.core/src/main/java/dk/sdu/se4/core/StartMenu.java
@@ -21,7 +21,9 @@ public class StartMenu implements Screen{
     private final static Logger logger = LoggerFactory.getLogger(GameScreen.class);
     private GameCore game;
     private Texture playbtn;
+    private Texture playActivebtn;
     private Texture exitbtn;
+    private Texture exitActivebtn;
     private Texture headline;
     private Texture background;
 
@@ -29,8 +31,10 @@ public class StartMenu implements Screen{
 
     public StartMenu(GameCore GameCore) {
         this.game = GameCore;
-        this.playbtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/Playbtn.png");
-        this.exitbtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/Exitbtn.png");
+        this.playbtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/play.png");
+        this.playActivebtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/playActive.png");
+        this.exitbtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/exit.png");
+        this.exitActivebtn = new Texture("../dk.sdu.se4.core/src/main/resources/img/exitActive.png");
         this.headline = new Texture("../dk.sdu.se4.core/src/main/resources/img/HeadLine.png");
         this.background = new Texture("../dk.sdu.se4.core/src/main/resources/img/Background.png");
     }
@@ -46,25 +50,29 @@ public class StartMenu implements Screen{
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         this.game.batch.begin();
-        this.game.batch.draw(this.background,(float)(this.game.width/4.2),(float) (this.game.height/12),400,500);
-        this.game.batch.draw(this.headline,(float)(this.game.width/3.8), (float) (this.game.height/1.333), 350,50);
-        this.game.batch.draw(this.playbtn,(float)(this.game.width/2.66),(float) (this.game.height/2));
-        this.game.batch.draw(this.exitbtn,(float)(this.game.width/2.66),(float) (this.game.height/3));
+        this.game.batch.draw(this.background,(float)(this.game.width/2)-(this.background.getWidth()/2),(float) (this.game.height/2)-(this.background.getHeight()/2));
+        this.game.batch.draw(this.headline,(float)(this.game.width/2)-(this.headline.getWidth()/2), (float) (this.game.height/1.333));
+
+
 
         if(Gdx.input.getX()< (this.game.width/2.66)+this.exitbtn.getWidth()&& Gdx.input.getX()>(this.game.width/2.66) &&
                 Gdx.input.getY()<(this.game.height/1.7)+this.exitbtn.getHeight() && Gdx.input.getY()>(this.game.height/1.7)){
-            this.game.batch.draw(this.exitbtn,(float)(this.game.width/2.66),(float) (this.game.height/3));
+            this.game.batch.draw(this.exitbtn,(float)(this.game.width/2)-(this.exitbtn.getWidth()/2),(float) (this.game.height/3));
             if(Gdx.input.isTouched()){
                 Gdx.app.exit();
             }
+        }else{
+            this.game.batch.draw(this.exitActivebtn,(float)(this.game.width/2)-(this.exitActivebtn.getWidth()/2),(float) (this.game.height/3));
         }
         if(Gdx.input.getX()< (this.game.width/2.66)+this.playbtn.getWidth()&& Gdx.input.getX()>(this.game.width/2.66) &&
                 Gdx.input.getY()<(this.game.height/2.7)+this.playbtn.getHeight() && Gdx.input.getY()>(this.game.height/2.7)){
-            this.game.batch.draw(this.playbtn,(float)(this.game.width/2.66),(float) (this.game.height/2));
+            this.game.batch.draw(this.playbtn,(float)(this.game.width/2)-(this.playbtn.getWidth()/2),(float) (this.game.height/2));
             if(Gdx.input.isTouched()){
                 this.dispose();
                 game.setScreen(new GameScreen(this.game));
             }
+        }else{
+            this.game.batch.draw(this.playActivebtn,(float)(this.game.width/2)-(this.playActivebtn.getWidth()/2),(float) (this.game.height/2));
         }
         this.game.batch.end();
         
