9a72fb9c6b37fee3462c9bd23967f19aac32f603
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-04-21 18:05:26 +0200

    add todos

--- dk.sdu.se4.collision/src/main/java/dk/sdu/se4/collision/CollisionServiceImpl.java
+++ dk.sdu.se4.collision/src/main/java/dk/sdu/se4/collision/CollisionServiceImpl.java
@@ -2,9 +2,10 @@ package dk.sdu.se4.collision;
 
 import dk.sdu.se4.collision.grid.Grid;
 import dk.sdu.se4.common.entity.Entity;
+import dk.sdu.se4.common.service.PostProcessorService;
 import dk.sdu.se4.commoncollision.CollisionService;
 
-public class CollisionServiceImpl implements CollisionService {
+public class CollisionServiceImpl implements CollisionService, PostProcessorService {
     // Hardcode size right now
     // TODO: use the game.size function to create grid (or whatever the function will be named)
     private final Grid grid = new Grid(100, 100, 800, 600);
@@ -22,5 +23,14 @@ public class CollisionServiceImpl implements CollisionService {
     @Override
     public void entityMoved(Entity entity, int previousX, int previousY) {
         grid.entityMoved(entity, previousX, previousY);
+    }
+
+    @Override
+    public void process() {
+        // TODO: Get all grid cells that have both friendly and enemies
+        // TODO: Loop through all friendly entities and then all enemy entities inside each grid cell
+        // TODO: Detect collision between collision parts of friendly and enemy
+        // TODO: Remove health from friendly if enemy attack collides with friendly
+        // TODO: Remove health from enemy if friendly attack collides with enemy
     }
 }
