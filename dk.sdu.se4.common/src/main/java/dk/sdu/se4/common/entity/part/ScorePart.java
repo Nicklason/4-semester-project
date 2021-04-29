64f8ca68951da4272e1d8282267c962b37414517
frederikstroem <frederikstroem@pm.me>
2021-04-29 12:52:39 +0200

    Add ScorePart.java
    Co-authored-by: lucassass <lucassass@live.com>

---
+++ dk.sdu.se4.common/src/main/java/dk/sdu/se4/common/entity/part/ScorePart.java
@@ -0,0 +1,50 @@
+package dk.sdu.se4.common.entity.part;
+
+import dk.sdu.se4.common.entity.Entity;
+
+/**
+ *
+ * @author Lucas
+ * @author Frederik
+ */
+public class ScorePart implements EntityPart {
+    private int currentScore;
+
+    /**
+     * Creates new instance of ScorePart
+     * @param score Score of entity
+     */
+    public ScorePart(int score) {
+        this.currentScore = currentScore;
+    }
+
+    /**
+     * Decrease score with amount
+     * @param amount The amount to decrease
+     */
+    public void decreaseScore(int amount){
+        this.currentScore -= amount;
+        this.scoreChanged();
+    }
+
+    /**
+     * Increase score with amount
+     * @param amount The amount to increase
+     */
+    public void increaseScore(int amount){
+        this.currentScore += amount;
+        this.scoreChanged();
+    }
+    
+    /**
+     * Method to call when score changes
+     */
+    private void scoreChanged() {
+        if (this.currentScore < 0) {
+            this.currentScore = 0;
+        }
+    }
+    
+    @Override
+    public void process(Entity entity) {}
+}
