440ed788dafd872eeb8fb9785f7e42d90391c2f3
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-05-18 19:23:40 +0200

    idk

--- dk.sdu.se4.weapon/src/main/java/dk/sdu/se4/weapon/WeaponProcessor.java
+++ dk.sdu.se4.weapon/src/main/java/dk/sdu/se4/weapon/WeaponProcessor.java
@@ -19,10 +19,10 @@ import java.io.File;
  * @author steff
  */
 public class WeaponProcessor extends WeaponCore implements ProcessorService {
-    
     private GameInput gameInput = null;
     boolean reloaded = false;
     int reloadTime = 2;
+    private SpritePart sp = new SpritePart("Bullets/bullet.png", 4,4,2);
     
     public void addGameInput(GameInput gameInput) {
         this.gameInput = gameInput;
@@ -31,7 +31,7 @@ public class WeaponProcessor extends WeaponCore implements ProcessorService {
     public void removeGameInput(GameInput gameInput) {
         this.gameInput = null;
     }
-    private SpritePart sp = new SpritePart("Bullets/bullet.png", 4,4,2);
+
     @Override
     public void process() {
         if (this.mapService != null) {
@@ -48,10 +48,12 @@ public class WeaponProcessor extends WeaponCore implements ProcessorService {
                             continue;
                         }
                         if(wp.getCurrentMagazine() == 0) {
-                          if(reloaded == false) {
+                            System.out.println("no bullets need reloading");
+                            if(reloaded == false) {
                                 tp.setTime(reloadTime);
                                 reloaded = true;
                             }
+
                             if(tp.getTime() <= 0) {
                                 wp.reload();
                                 float fireRate = wp.getFireRate();
@@ -60,28 +62,27 @@ public class WeaponProcessor extends WeaponCore implements ProcessorService {
                             }
                             continue;
                         }
+
                         if(tp.getTime() <= 0) {
                             wp.removeBullet();
                             tp.setTime((float) 0.2);
 
-                        // Add bullet here
-                        Entity bullet = new Bullet();
-                        bullet.addPart(new PositionPart(pp.getX(), pp.getY()));
-                        bullet.addPart(sp);
-                        bullet.addPart(new TimePart(3));
-                        bullet.addPart(new DirectionPart(dp.getLookingUp(), dp.getLookingDown(), dp.getLookingLeft(), dp.getLookingRight()));
-                        bullet.addPart(new MovingPart(4));
-                        bullet.addPart(new CollisionPart(4, 4));
-                        bullet.addPart(new FriendlyPart(true));
-                        this.mapService.addEntity(bullet);
-                    }
-                    else {
+                            // Add bullet here
+                            Entity bullet = new Bullet();
+                            bullet.addPart(new PositionPart(pp.getX(), pp.getY()));
+                            bullet.addPart(sp);
+                            bullet.addPart(new TimePart(3));
+                            bullet.addPart(new DirectionPart(dp.getLookingUp(), dp.getLookingDown(), dp.getLookingLeft(), dp.getLookingRight()));
+                            bullet.addPart(new MovingPart(4));
+                            bullet.addPart(new CollisionPart(4, 4));
+                            bullet.addPart(new FriendlyPart(true));
+                            this.mapService.addEntity(bullet);
+                        }
+                    } else {
                         // Add melee here
                     }
                 }
             }
-
         }
     }
-
 }
